25f7e5e47c0fec8e2558a789e43cabc248211148
Chandan Kumar <97844907+ChandanKr16@users.noreply.github.com>
2025-07-23 22:54:48 +0530

    Create 15-3sum

---
+++ 15-3sum/15-3sum.java
@@ -0,0 +1,57 @@
+// Last updated: 7/23/2025, 10:54:48 PM
+class Solution {
+    public List<List<Integer>> threeSum(int[] arr) {
+        //-4 -1 -1 0 1 2
+        List<List<Integer>> result = new ArrayList<>();
+
+        Arrays.sort(arr);
+        int last = arr.length-1;
+        int start = 1;
+
+        for(int i = 0; i < arr.length; i++){
+
+            if(i > 0 && arr[i] == arr[i-1])
+                continue;
+
+            start = i+1;
+           last = arr.length-1;
+
+            
+            while(start < last){
+
+                int sum = arr[start] + arr[i] + arr[last];
+
+                if(sum > 0){
+                    last--;
+                }
+                else if(sum < 0){
+                    start++;
+                }
+                else{
+
+                    result.add(new ArrayList<>(List.of(arr[i], arr[start], arr[last])));
+
+                    start++;
+                    last--;
+
+                while(start < last && arr[start] == arr[start-1]){
+                    start++;
+                }
+
+                while(start < last && arr[last] == arr[last+1]){
+                    last--;
+                }
+
+            }
+
+            }
+            
+
+            
+            
+        }
+
+        return result;   
+
+    }
+}
