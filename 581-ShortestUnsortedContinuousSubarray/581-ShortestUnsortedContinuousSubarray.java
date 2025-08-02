18fa8636d0eb40c11a1441ee1b144f6c52da5cb9
Chandan Kumar <97844907+ChandanKr16@users.noreply.github.com>
2025-08-02 19:59:15 +0530

    File updated at 8/2/2025, 7:59:15 PM

--- 581-ShortestUnsortedContinuousSubarray/581-ShortestUnsortedContinuousSubarray.java
+++ 581-ShortestUnsortedContinuousSubarray/581-ShortestUnsortedContinuousSubarray.java
@@ -1,15 +1,29 @@
-// Last updated: 8/2/2025, 7:23:18 PM
+// Last updated: 8/2/2025, 7:59:15 PM
 class Solution {
-    public int findUnsortedSubarray(int[] A) {
-        
-      int n = A.length, beg = -1, end = -2, min = A[n-1], max = A[0];
-    for (int i=1;i<n;i++) {
-      max = Math.max(max, A[i]);
-      min = Math.min(min, A[n-1-i]);
-      if (A[i] < max) end = i;
-      if (A[n-1-i] > min) beg = n-1-i; 
-    }
-    return end - beg + 1;
+    public int findUnsortedSubarray(int[] nums) {
+
+        int left = -1;
+        int right = -2;
+        int max = nums[0];
+        int min = nums[nums.length-1];
+
+        for(int i = 1; i < nums.length; i++){
+            max = Math.max(max, nums[i]);
+            min = Math.min(min, nums[nums.length-1-i]);
+
+            if(nums[i] < max){
+                right = i;
+            } 
+
+            if(nums[nums.length-i-1] > min){
+                left = nums.length-1-i;
+            }
+
+        }
+
+       
+
+        return right - left + 1;
 
     }
 }
