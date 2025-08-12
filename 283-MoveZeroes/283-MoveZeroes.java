785c68110435046aded529a7ccc9611746be1fb8
Chandan Kumar <97844907+ChandanKr16@users.noreply.github.com>
2025-08-12 20:30:34 +0530

    File updated at 8/12/2025, 8:30:34 PM

--- 283-MoveZeroes/283-MoveZeroes.java
+++ 283-MoveZeroes/283-MoveZeroes.java
@@ -1,18 +1,20 @@
-// Last updated: 7/23/2025, 10:54:08 PM
+// Last updated: 8/12/2025, 8:30:34 PM
 class Solution {
     public void moveZeroes(int[] nums) {
-        int start = 0;
+
+        if(nums.length <= 1) return;
+
+        int insertPointer = 0;
         int i = 0;
 
-        while(start < nums.length){
+        while(i < nums.length){
 
-            if(nums[start] != 0){
-                int temp = nums[i];
-                nums[i] = nums[start];
-                nums[start] = temp;
-                i++;
+            if(nums[i] != 0){
+                int temp = nums[insertPointer];
+                nums[insertPointer++] = nums[i];
+                nums[i] = temp;                 
             }
-            start++;
+            i++;
 
         }
     }
