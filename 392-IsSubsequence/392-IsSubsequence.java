22a1f69450666721706efa75644fbf8439cab822
Chandan Kumar <97844907+ChandanKr16@users.noreply.github.com>
2025-08-13 22:47:52 +0530

    File updated at 8/13/2025, 10:47:52 PM

--- 392-IsSubsequence/392-IsSubsequence.java
+++ 392-IsSubsequence/392-IsSubsequence.java
@@ -1,15 +1,13 @@
-// Last updated: 8/13/2025, 10:40:47 PM
+// Last updated: 8/13/2025, 10:47:52 PM
 class Solution {
     public boolean isSubsequence(String s, String t) {
         
         int count = 0;
         int j = 0;
+        int i = 0;
 
-        if(s.length() == 0) return true;
-
-        for(int i = 0; i < t.length(); i++){
-
-            if(count == s.length()) return true;
+        while(i < t.length() && j < s.length()){
+            
 
             char tChar = t.charAt(i);
             char sChar = s.charAt(j);
@@ -18,6 +16,7 @@ class Solution {
                 j++;
                 count++;
             }
+            i++;
 
         }
 
