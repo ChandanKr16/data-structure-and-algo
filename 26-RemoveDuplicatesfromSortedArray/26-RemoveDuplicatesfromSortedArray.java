bd6e31fdd4c7fa3c670ded8d02abac61345bec4b
Chandan Kumar <97844907+ChandanKr16@users.noreply.github.com>
2025-08-13 19:33:40 +0530

    File updated at 8/13/2025, 7:33:40 PM

--- 26-RemoveDuplicatesfromSortedArray/26-RemoveDuplicatesfromSortedArray.java
+++ 26-RemoveDuplicatesfromSortedArray/26-RemoveDuplicatesfromSortedArray.java
@@ -1,21 +1,23 @@
-// Last updated: 8/12/2025, 8:04:38 PM
+// Last updated: 8/13/2025, 7:33:40 PM
 class Solution {
     public int removeDuplicates(int[] nums) {
+        
         int insertPointer = 1;
         int i = 1;
         int count = 1;
+
         while(i < nums.length){
 
             if(nums[i] != nums[i-1]){
-                nums[insertPointer++] = nums[i]; 
+                nums[insertPointer++] = nums[i];
                 count++;
             }
-            
 
             i++;
-
         }
 
+
         return count;
+
     }
 }
