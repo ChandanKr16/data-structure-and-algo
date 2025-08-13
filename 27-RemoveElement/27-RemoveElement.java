078ba67ec9208ff272594c9151f4231a21e4937b
Chandan Kumar <97844907+ChandanKr16@users.noreply.github.com>
2025-08-13 21:37:57 +0530

    File updated at 8/13/2025, 9:37:57 PM

--- 27-RemoveElement/27-RemoveElement.java
+++ 27-RemoveElement/27-RemoveElement.java
@@ -1,21 +1,17 @@
-// Last updated: 7/23/2025, 10:54:40 PM
+// Last updated: 8/13/2025, 9:37:57 PM
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int start = 0;
+        int insertPointer = 0;
         int i = 0;
         int count = 0;
-        while(start < nums.length){
 
-            if(nums[start] != val){
-                int temp = nums[start];
-                nums[start] = nums[i];
-                nums[i] = temp;
-                i++;
-           
+        while(i < nums.length){
+            if(nums[i] != val){
+                
+                nums[insertPointer++] = nums[i];
                 count++;
             }
-        start++;
-
+            i++;
         }
 
         return count;
