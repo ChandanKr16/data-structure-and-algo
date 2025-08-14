dd1b6a54df68987768651aeedab09bcda41be2b7
Chandan Kumar <97844907+ChandanKr16@users.noreply.github.com>
2025-08-14 22:07:43 +0530

    File updated at 8/14/2025, 10:07:43 PM

--- 75-SortColors/75-SortColors.java
+++ 75-SortColors/75-SortColors.java
@@ -1,27 +1,23 @@
-// Last updated: 8/13/2025, 9:30:26 PM
+// Last updated: 8/14/2025, 10:07:43 PM
 class Solution {
     public void sortColors(int[] nums) {
-        int left = 0;
-        int right = nums.length-1;
-        int mid = 0;
+       int start = 0, mid = 0, end = nums.length-1;
 
-        while(mid <= right){
-            if(nums[mid] == 0){
-                nums[mid] = nums[left];
-                nums[left] = 0;
-                left++;
-                mid++;
-            }
-            else if(nums[mid] == 2){
-                nums[mid] = nums[right];
-                nums[right] = 2;
-                right--;
-            }
-            else{
-                mid++;
-            }
-        }
+       while(mid <= end){
 
-      
+             if(nums[mid] == 0){
+                nums[mid] = nums[start];
+                nums[start] = 0;
+                start++;
+                mid++;
+             }
+             else if(nums[mid] == 2){
+                nums[mid] = nums[end];
+                nums[end] = 2;
+                end--;
+             }
+             else{mid++;}
+       }
+
     }
 }
