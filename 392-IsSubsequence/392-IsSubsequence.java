31a3e46066de5147c4c5d67342e2f981f42254a8
Chandan Kumar <97844907+ChandanKr16@users.noreply.github.com>
2025-08-14 22:21:53 +0530

    File updated at 8/14/2025, 10:21:53 PM

--- 392-IsSubsequence/392-IsSubsequence.java
+++ 392-IsSubsequence/392-IsSubsequence.java
@@ -1,24 +1,27 @@
-// Last updated: 8/13/2025, 10:47:52 PM
+// Last updated: 8/14/2025, 10:21:53 PM
 class Solution {
     public boolean isSubsequence(String s, String t) {
         
         int count = 0;
-        int j = 0;
         int i = 0;
+        int j = 0;
 
-        while(i < t.length() && j < s.length()){
-            
+        while(i < s.length() && j < t.length()){
 
-            char tChar = t.charAt(i);
-            char sChar = s.charAt(j);
+            char charI = s.charAt(i);
+            char charT = t.charAt(j);
 
-            if(tChar == sChar){
+            if(charI == charT){
+                i++;
                 j++;
                 count++;
             }
-            i++;
+            else{
+                j++;
+            }
 
         }
+        
 
         return count == s.length();
 
