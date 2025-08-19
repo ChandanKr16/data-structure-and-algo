f025f5129a1a9be90b43b7cfac31f57991776d07
Chandan Kumar <97844907+ChandanKr16@users.noreply.github.com>
2025-08-19 08:05:49 +0530

    File updated at 8/19/2025, 8:05:49 AM

--- 42-TrappingRainWater/42-TrappingRainWater.java
+++ 42-TrappingRainWater/42-TrappingRainWater.java
@@ -1,29 +1,27 @@
-// Last updated: 8/16/2025, 8:09:38 PM
+// Last updated: 8/19/2025, 8:05:49 AM
 class Solution {
     public int trap(int[] height) {
-    
-        int size = height.length;
         int trappedWater = 0;
-        int leftPrefix[] = new int[size];
-        int rightPrefix[] = new int[size];
-
+        int n = height.length;
+        int leftPrefix[] = new int[n];
+        int rightPrefix[] = new int[n];
         leftPrefix[0] = height[0];
-        rightPrefix[size-1] = height[size-1];
+        rightPrefix[n - 1] = height[n - 1];
 
-        for(int i = 1; i < size; i++){
-            leftPrefix[i] = Math.max(height[i], leftPrefix[i-1]);
+        for (int i = 1; i < n; i++) {
+            leftPrefix[i] = Math.max(leftPrefix[i - 1], height[i]);
         }
 
-        for(int i = size - 2; i >= 0; i--){
-            rightPrefix[i] = Math.max(height[i], rightPrefix[i+1]);
+
+        for(int i = n-2; i >= 0; i--){
+            rightPrefix[i] = Math.max(rightPrefix[i+1], height[i]);
         }
 
-        for(int i = 0; i < size; i++){
+        for(int i = 0; i < n; i++){
             trappedWater += Math.min(leftPrefix[i], rightPrefix[i]) - height[i];
         }
 
-        
-
         return trappedWater;
+
     }
 }
