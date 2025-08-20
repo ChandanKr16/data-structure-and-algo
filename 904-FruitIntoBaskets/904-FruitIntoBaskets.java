28fb1992744f165b6e2b877b485b029204d614cc
Chandan Kumar <97844907+ChandanKr16@users.noreply.github.com>
2025-08-20 20:05:51 +0530

    File updated at 8/20/2025, 8:05:51 PM

--- 904-FruitIntoBaskets/904-FruitIntoBaskets.java
+++ 904-FruitIntoBaskets/904-FruitIntoBaskets.java
@@ -1,34 +1,35 @@
-// Last updated: 8/19/2025, 8:19:55 PM
+// Last updated: 8/20/2025, 8:05:51 PM
 class Solution {
     public int totalFruit(int[] fruits) {
-
         int maxCount = 0;
-        Map<Integer, Integer> map = new HashMap<>();
         int count = 0;
         int start = 0;
+        int end = 0;
+        
+        Map<Integer, Integer> map = new HashMap<>();
 
-        for(int i = 0; i < fruits.length; i++){
-            map.put(fruits[i], map.getOrDefault(fruits[i], 0)+1);
+
+        while(end < fruits.length){
+            
+            map.put(fruits[end], map.getOrDefault(fruits[end], 0)+1);
             count++;
 
-            //if(map.size() <= 2){
-                
-        //    }    
-
             while(map.size() > 2){
-                
                 map.put(fruits[start], map.get(fruits[start])-1);
                 count--;
-
-                if(map.get(fruits[start]) == 0){
+                if(map.get(fruits[start]) == 0)
                     map.remove(fruits[start]);
-                }
                 start++;
-            }    
+            }
+            maxCount = Math.max(maxCount, count);
 
-            maxCount = Math.max(maxCount, count);    
+            end++;
+
         }
 
         return maxCount;
+        
+
+       
     }
 }
