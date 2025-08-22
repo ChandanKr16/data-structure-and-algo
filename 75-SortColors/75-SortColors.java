0d70a7a6ca45eef04dd4985c01307de29034912f
Chandan Kumar <97844907+ChandanKr16@users.noreply.github.com>
2025-08-22 20:00:42 +0530

    File updated at 8/22/2025, 8:00:41 PM

--- 75-SortColors/75-SortColors.java
+++ 75-SortColors/75-SortColors.java
@@ -1,23 +1,52 @@
-// Last updated: 8/14/2025, 10:07:43 PM
+// Last updated: 8/22/2025, 8:00:41 PM
 class Solution {
-    public void sortColors(int[] nums) {
-       int start = 0, mid = 0, end = nums.length-1;
 
-       while(mid <= end){
+    public String minWindow(String str, String pattern) {
+        int minLen = Integer.MAX_VALUE;
+        int start = 0;
+        int startIdx = 0;
+        int matched = 0;
 
-             if(nums[mid] == 0){
-                nums[mid] = nums[start];
-                nums[start] = 0;
-                start++;
-                mid++;
-             }
-             else if(nums[mid] == 2){
-                nums[mid] = nums[end];
-                nums[end] = 2;
-                end--;
-             }
-             else{mid++;}
-       }
+        Map<Character, Integer> map = new HashMap<>();
 
+        for(char ch : pattern.toCharArray()){
+            map.put(ch, map.getOrDefault(ch, 0)+1);
+        }
+
+                    Map<Character, Integer> map2 = new HashMap<>();
+
+       
+        for(int end = 0; end < str.length(); end++){
+            char ch = str.charAt(end);
+
+
+            map2.put(ch, map2.getOrDefault(ch, 0)+1);
+
+            if(map.containsKey(ch) && map2.get(ch).intValue() == map.get(ch).intValue()){
+                matched++;
+            }
+
+            while(matched == map.size()){
+
+                if(end - start + 1 < minLen){
+                    minLen = Math.min(end-start+1, minLen);
+                    startIdx = start;
+                }
+
+                char leftCh = str.charAt(start++);
+
+                map2.put(leftCh, map2.getOrDefault(leftCh, 0)-1);
+
+                if(map2.get(leftCh) < map.getOrDefault(leftCh, 0)){
+                    matched--;
+                }
+                
+
+            }
+
+
+        }
+
+        return minLen == Integer.MAX_VALUE  ? "" : str.substring(startIdx, startIdx+minLen);
     }
 }
