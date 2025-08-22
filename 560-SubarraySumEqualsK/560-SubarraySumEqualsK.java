33455332fef4132ae7517819fe399a15afa48743
Chandan Kumar <97844907+ChandanKr16@users.noreply.github.com>
2025-08-22 18:06:08 +0530

    File updated at 8/22/2025, 6:06:08 PM

--- 560-SubarraySumEqualsK/560-SubarraySumEqualsK.java
+++ 560-SubarraySumEqualsK/560-SubarraySumEqualsK.java
@@ -1,52 +1,15 @@
-// Last updated: 8/22/2025, 4:01:54 PM
+// Last updated: 8/22/2025, 6:06:08 PM
 class Solution {
 
 
-       private int slidingWindowAtMost(int[] nums, int goal) {
-        int start = 0, currentSum = 0, totalCount = 0;
-
-        // Iterate through the array using a sliding window approach
-        for (int end = 0; end < nums.length; end++) {
-            currentSum += nums[end];
-
-            // Adjust the window by moving the start pointer to the right
-            // until the sum becomes less than or equal to the goal
-            while (start <= end && currentSum > goal) {
-                currentSum -= nums[start++];
-            }
-
-            // Update the total count by adding the length of the current subarray
-            totalCount += end - start + 1;
-        }
-        return totalCount;
-    }
+     
 
     public int subarraySum(int[] nums, int goal) {
-        // int sum = 0;
-        // int count = 0;
-        // int left = 0, right = 0;
-
-        // while(right < nums.length){
-        //     sum += nums[right];
-            
-        //     if(sum == k){
-        //         count += right - left + 1;
-        //     }
-
-        //     while(sum > k && left <= right){
-        //         sum -= nums[left++];
-        //     }
-
-            
-        //     right++;
-
-        // }
-
-        // return count;
+        
 
           int totalCount = 0;
         int currentSum = 0;
-        // {prefix: number of occurrence}
+        
         Map<Integer, Integer> freq = new HashMap<>(); // To store the frequency of prefix sums
 
         for (int num : nums) {
@@ -67,6 +30,6 @@ class Solution {
 
 
 
-       // return slidingWindowAtMost(nums, k) - slidingWindowAtMost(nums, k-1);
+    
     }
 }
