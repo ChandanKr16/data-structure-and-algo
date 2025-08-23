bfc3415753fabcaf3fec1336f45fd43bc8b83ce6
Chandan Kumar <97844907+ChandanKr16@users.noreply.github.com>
2025-08-23 08:30:44 +0530

    File updated at 8/23/2025, 8:30:44 AM

--- 930-BinarySubarraysWithSum/930-BinarySubarraysWithSum.java
+++ 930-BinarySubarraysWithSum/930-BinarySubarraysWithSum.java
@@ -1,22 +1,25 @@
-// Last updated: 8/23/2025, 8:30:05 AM
+// Last updated: 8/23/2025, 8:30:44 AM
 class Solution {
     public int numSubarraysWithSum(int[] nums, int goal) {
         int count = 0;
         int sum = 0;
-        Map<Integer, Integer> map = new HashMap<>();
+        Map<Integer, Integer> prefixSumCount = new HashMap<>();
+        prefixSumCount.put(0, 1);
+
 
         for(int num : nums){
             sum += num;
 
-            if(sum == goal) count++;
+           // if(sum == goal) ++count;
 
-            if(map.containsKey(sum - goal)){
-                count +=  map.get(sum - goal);
+            if(prefixSumCount.containsKey(sum - goal)){
+                count += prefixSumCount.get(sum-goal);
             }
 
-            map.put(sum, map.getOrDefault(sum, 0)+1);
-        }
+            prefixSumCount.put(sum, prefixSumCount.getOrDefault(sum, 0)+1);
 
+
+        }
 
         return count;
     }
