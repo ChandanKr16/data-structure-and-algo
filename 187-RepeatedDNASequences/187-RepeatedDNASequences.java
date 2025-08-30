ec9478d2eb65dfc539b97c213ddb37952736a3c9
Chandan Kumar <97844907+ChandanKr16@users.noreply.github.com>
2025-08-30 20:16:50 +0530

    File updated at 8/30/2025, 8:16:50 PM

--- 187-RepeatedDNASequences/187-RepeatedDNASequences.java
+++ 187-RepeatedDNASequences/187-RepeatedDNASequences.java
@@ -1,23 +1,23 @@
-// Last updated: 8/30/2025, 7:55:07 PM
+// Last updated: 8/30/2025, 8:16:50 PM
 class Solution {
     public List<String> findRepeatedDnaSequences(String s) {
         List<String> result = new ArrayList<>();
         int start = 0;
         Map<String, Integer> map = new HashMap<>();
-        Set<String> set = new HashSet<>();
+        
 
         for(int i = 0; i <= s.length()-10; i++){
             String substr = s.substring(start, i+10);  
 
             map.put(substr, map.getOrDefault(substr, 0)+1);
 
-            if(map.get(substr) > 1){
-                set.add(substr);
+            if(map.get(substr) == 2){
+                result.add(substr);
             } 
             start++; 
         }
 
-        result.addAll(set);
+       
 
         return result;
     }
