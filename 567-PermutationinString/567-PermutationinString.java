6133280419b47c58c1ae3db9ff90fdde44c714ed
Chandan Kumar <97844907+ChandanKr16@users.noreply.github.com>
2025-09-02 22:30:20 +0530

    File updated at 9/2/2025, 10:30:20 PM

--- 567-PermutationinString/567-PermutationinString.java
+++ 567-PermutationinString/567-PermutationinString.java
@@ -1,33 +1,43 @@
-// Last updated: 8/31/2025, 2:43:31 PM
+// Last updated: 9/2/2025, 10:30:20 PM
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
 
-        int end = 0;
-        int start = 0;
-        Map<Character, Integer> pCount = new HashMap<>();
-        Map<Character, Integer> sCount = new HashMap<>();
+        if(s1.length() > s2.length()) return false;
+
+        int start = 0, end = 0;
+        Map<Character, Integer> pattern = new HashMap<>();
+        Map<Character, Integer> freq = new HashMap<>();
 
         for(char ch : s1.toCharArray()){
-            pCount.put(ch, pCount.getOrDefault(ch, 0)+1);
+            pattern.put(ch, pattern.getOrDefault(ch, 0)+1);
+        }
+
+        while(end < s1.length()-1){
+            char ch = s2.charAt(end++);
+            freq.put(ch, freq.getOrDefault(ch, 0)+1);
         }
 
         while(end < s2.length()){
 
-            char ch = s2.charAt(end);
-            sCount.put(ch, sCount.getOrDefault(ch, 0)+1);
+             char ch = s2.charAt(end);
+            freq.put(ch, freq.getOrDefault(ch, 0)+1);
 
-            if(end - start + 1 > s1.length()){
+            if(freq.equals(pattern)) return true;
+
+            while(end - start + 1 >= s1.length()){
+
                 char startCh = s2.charAt(start++);
-                sCount.put(startCh, sCount.get(startCh)-1);
-                if(sCount.get(startCh) == 0) 
-                    sCount.remove(startCh);
+                freq.put(startCh, freq.get(startCh)-1);
+
+                if(freq.get(startCh)== 0)
+                    freq.remove(startCh);               
+
             }
 
-             if(pCount.equals(sCount)) 
-                    return true;     
-           
             end++;
+           
         }
+       
         
         return false;
     }
