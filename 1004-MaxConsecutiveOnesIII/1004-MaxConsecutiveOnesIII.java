879e0cb72617204f2cf601b5a8383420c0201703
Chandan Kumar <97844907+ChandanKr16@users.noreply.github.com>
2025-09-04 07:48:39 +0530

    File updated at 9/4/2025, 7:48:38 AM

--- 1004-MaxConsecutiveOnesIII/1004-MaxConsecutiveOnesIII.java
+++ 1004-MaxConsecutiveOnesIII/1004-MaxConsecutiveOnesIII.java
@@ -1,14 +1,12 @@
-// Last updated: 9/4/2025, 7:48:25 AM
+// Last updated: 9/4/2025, 7:48:38 AM
 class Solution {
     public int longestOnes(int[] nums, int k) {
         int maxLen = 0, start = 0, end = 0, n = nums.length;
         int oneCount = 0;
-        Map<Integer, Integer> map = new HashMap<>();
 
         while(end < n){
 
-            map.put(nums[end], map.getOrDefault(nums[end], 0)+1);
-
+            
             if(nums[end] == 1) oneCount++;
 
             
