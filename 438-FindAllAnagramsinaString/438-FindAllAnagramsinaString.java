1508eb57fb6db392f349fe408ab3c6908ad7a30b
Chandan Kumar <97844907+ChandanKr16@users.noreply.github.com>
2025-09-05 12:45:32 +0530

    File updated at 9/5/2025, 12:45:32 PM

--- 438-FindAllAnagramsinaString/438-FindAllAnagramsinaString.java
+++ 438-FindAllAnagramsinaString/438-FindAllAnagramsinaString.java
@@ -1,4 +1,4 @@
-// Last updated: 9/2/2025, 10:10:31 PM
+// Last updated: 9/5/2025, 12:45:32 PM
 class Solution {
     
     public List<Integer> findAnagrams(String s, String p) {
@@ -14,8 +14,12 @@ class Solution {
             pCount.put(ch, pCount.getOrDefault(ch, 0)+1);
         }
 
-        for(; end < p.length()-1; end++){
+        for(; end < p.length(); end++){
             sCount.put(s.charAt(end), sCount.getOrDefault(s.charAt(end), 0)+1);
+        }
+
+        if(sCount.equals(pCount)){
+            result.add(start);
         }
 
         while(end < n){
@@ -23,17 +27,18 @@ class Solution {
 
             sCount.put(ch, sCount.getOrDefault(ch, 0)+1);
 
-            if(sCount.equals(pCount)){
-                result.add(start);
-            }
 
             
-            while(end - start + 1 >= p.length()){
+           // while(end - start + 1 >= p.length()){
                 char startCh = s.charAt(start++);
                 sCount.put(startCh, sCount.get(startCh)-1);
 
                 if(sCount.get(startCh) == 0)
                     sCount.remove(startCh);
+          //  }
+
+            if(sCount.equals(pCount)){
+                result.add(start);
             }
 
             end++;
