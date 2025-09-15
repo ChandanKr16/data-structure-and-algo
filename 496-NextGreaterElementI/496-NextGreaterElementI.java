dae0e3ff8dc93e3dc6b53dc4f0ac7c256f629b6b
Chandan Kumar <97844907+ChandanKr16@users.noreply.github.com>
2025-09-15 07:54:00 +0530

    File updated at 9/15/2025, 7:54:00 AM

--- 496-NextGreaterElementI/496-NextGreaterElementI.java
+++ 496-NextGreaterElementI/496-NextGreaterElementI.java
@@ -1,29 +1,29 @@
-// Last updated: 9/15/2025, 7:28:33 AM
+// Last updated: 9/15/2025, 7:54:00 AM
 class Solution {
+
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+       
         int result[] = new int[nums1.length];
-        Map<Integer, Integer> nextGreater = new HashMap<>();
-        Stack<Integer> stack = new Stack<>();
+        int j = 0;
+        for(int num : nums1){
+            boolean isFound = false;
+            result[j] = -1;
+            for(int i = 0; i < nums2.length; i++){
 
-        for(int num : nums2){
+                if(isFound == true && nums2[i] > num){
+                    result[j] = nums2[i];
+                    break;
+                }
 
-            while(!stack.isEmpty() && num > stack.peek()){
-                nextGreater.put(stack.pop(), num);
+                if(nums2[i] == num){
+                    isFound = true;
+                }
+
             }
-            stack.push(num);
-        }
-
-        while(!stack.isEmpty()){
-
-            nextGreater.put(stack.pop(), -1);
+            j++;
 
         }
 
-        for(int i = 0; i < nums1.length; i++){
-            result[i] = nextGreater.get(nums1[i]);
-        }
-                
-        
         return result;
     }
 }
