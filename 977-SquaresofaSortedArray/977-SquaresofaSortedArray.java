007dd5236bc132a4b54528bc3363fcc67b7cbd3a
Chandan Kumar <97844907+ChandanKr16@users.noreply.github.com>
2026-04-03 11:45:33 +0530

    File updated at 4/3/2026, 11:45:32 AM

--- 977-SquaresofaSortedArray/977-SquaresofaSortedArray.java
+++ 977-SquaresofaSortedArray/977-SquaresofaSortedArray.java
@@ -1,26 +1,26 @@
-// Last updated: 8/13/2025, 7:45:33 PM
-class Solution {
-    public int[] sortedSquares(int[] nums) {
-        int sortedArr[] = new int[nums.length];
-
-
-        int left = 0, right = nums.length-1, i = nums.length-1;
-
-        while(left <= right){
-            int leftSq = nums[left] * nums[left];
-            int rightSq = nums[right] * nums[right];
-
-            if(leftSq > rightSq){
-                sortedArr[i--] = leftSq;
-                left++;
-            }
-            else{
-                sortedArr[i--] = rightSq;
-                right--;
-            }
-        }
-     
-
-        return sortedArr;
-    }
-}
+// Last updated: 4/3/2026, 11:45:32 AM
+1class Solution {
+2    public int[] sortedSquares(int[] nums) {
+3        int left = 0, right = nums.length-1;
+4
+5        int sortedSquares[] = new int[nums.length];
+6        int insertPointer = right;
+7
+8        while(left <= right){
+9            int leftSq = nums[left]*nums[left];
+10            int rightSq = nums[right]*nums[right];
+11
+12            if(leftSq > rightSq){
+13                sortedSquares[insertPointer--] = leftSq;
+14                left++;
+15            }
+16            else {
+17                sortedSquares[insertPointer--] = rightSq;
+18                right--;
+19            }
+20            
+21        }
+22
+23        return sortedSquares;
+24    }
+25}
