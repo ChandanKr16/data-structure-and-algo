e9207075f98550187594450b78fc7517d61b12ce
Chandan Kumar <97844907+ChandanKr16@users.noreply.github.com>
2026-04-07 20:54:23 +0530

    File updated at 4/7/2026, 8:54:23 PM

--- 881-BoatstoSavePeople/881-BoatstoSavePeople.java
+++ 881-BoatstoSavePeople/881-BoatstoSavePeople.java
@@ -1,34 +1,28 @@
-// Last updated: 9/5/2025, 7:27:08 PM
-class Solution {
-    public int numRescueBoats(int[] people, int limit) {
-        int count = 0;
-        int start = 0;
-        int end = people.length-1;
-        int sum = 0;
-
-        Arrays.sort(people);
-
-        while(start <= end){
-
-            
-           if(people[start] + people[end] <= limit){
-                start++;
-                end--;
-                count++;
-            }
-            else {
-                count++;
-                end--;
-            }
-
-            
-             
-           
-        }
-
-        
-
-
-        return count;
-    }
-}
+// Last updated: 4/7/2026, 8:54:23 PM
+1class Solution {
+2    public int numRescueBoats(int[] people, int limit) {
+3
+4        Arrays.sort(people);
+5
+6        int boatCount = 0, left = 0, right = people.length-1;
+7
+8        while(left <= right){
+9            int sum = people[left] + people[right];
+10
+11            if(sum <= limit){
+12                boatCount++;
+13                left++;
+14                right--;
+15            }
+16            else if(sum > limit){
+17                boatCount++;
+18                right--;
+19            }
+20        }
+21
+22        if(right-left == 0) boatCount++;
+23        
+24
+25        return boatCount;
+26    }
+27}
